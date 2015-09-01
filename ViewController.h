4cd036e8d4f0dab845c71e52e6f614282313fd23
ParameswarVellachu <appsdeveloperinfo@gmail.com>
2015-09-01 18:19:02 +0530

    Create ViewController.h

---
+++ ViewController.h
@@ -0,0 +1,22 @@
+//
+//  ViewController.h
+//  LearnStoryBoardPgm
+//
+//  Created by Parames on 31/08/15.
+//  Copyright (c) 2015 Softcraft Systems and Solutions Private Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+-(IBAction)onNT:(id)sender;
+-(IBAction)onOT:(id)sender;
+
+-(IBAction)onSearch:(id)sender;
+-(IBAction)onWS:(id)sender;
+
+-(IBAction)onSettings:(id)sender;
+-(IBAction)onAbout:(id)sender;
+
+@end
